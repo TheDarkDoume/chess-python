24596df8cca9e458e7a4c4465a52f6d61eb6c83d
TheDarkDoume <116193882+TheDarkDoume@users.noreply.github.com>
2025-06-22 09:53:42 +0100

    using class for board
    why not looks fancy

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-def newgame(): # generates default chessboard
-    return [
-        ["r","k","b","q","k","b","k","r"],
-        ["p" for i in range(8)],
-        [" " for i in range(8)],
-        [" " for i in range(8)],
-        [" " for i in range(8)],
-        [" " for i in range(8)],
-        ["P" for i in range (8)],
-        ["R","K","B","Q","K","B","K","R"]
-    ]
 
-def turn():
-    while True:
-        from_pos = input("enter the coordinates for the piece you want to move: ").lower()
-        to_pos = input("enter the coordinates for the next location: ").lower()
-        try:
-            for move in zip(from_pos,to_pos):
-                if move[0] in ["a","b","c","d","e","f","g","h"]:
-                    continue
-                elif int(move[1:]) in range(1,9):
-                    continue
-                raise ValueError
-        except ValueError:
-            print("invalid move")
-            continue
-        if valid_move(from_pos,to_pos):
-            move(from_pos,to_pos)
+class game:
+    def __init__(self):
+        self.turn = "w"
+        self.newgame()
 
-turn()
+    def newgame(self): # generates default chessboard
+        self.board =  [
+            ["r","k","b","q","k","b","k","r"],
+            ["p" for i in range(8)],
+            [" " for i in range(8)],
+            [" " for i in range(8)],
+            [" " for i in range(8)],
+            [" " for i in range(8)],
+            ["P" for i in range (8)],
+            ["R","K","B","Q","K","B","K","R"]
+            ]
+        
+        
+    def valid_move(self):
+        
+
+
+    def turn(self):
+        while True:
+            from_pos = input("enter the coordinates for the piece you want to move: ").lower()
+            to_pos = input("enter the coordinates for the next location: ").lower()
+            try:
+                for move in zip(from_pos,to_pos):
+                    if move[0] in ["a","b","c","d","e","f","g","h"]:
+                        continue
+                    elif int(move[1:]) in range(1,9):
+                        continue
+                    raise ValueError
+            except ValueError:
+                print("invalid move")
+                continue
+            if self.valid_move():
+                self.move()
