7c33207307f005830b2f0157e21c680ea9aba999
TheDarkDoume <116193882+TheDarkDoume@users.noreply.github.com>
2025-06-22 07:18:32 +0100

    basic start

---
+++ main.py
@@ -0,0 +1,30 @@
+def newgame(): # generates default chessboard
+    return [
+        ["r","k","b","q","k","b","k","r"],
+        ["p" for i in range(8)],
+        [" " for i in range(8)],
+        [" " for i in range(8)],
+        [" " for i in range(8)],
+        [" " for i in range(8)],
+        ["P" for i in range (8)],
+        ["R","K","B","Q","K","B","K","R"]
+    ]
+
+def turn():
+    while True:
+        from_pos = input("enter the coordinates for the piece you want to move: ").lower()
+        to_pos = input("enter the coordinates for the next location: ").lower()
+        try:
+            for move in zip(from_pos,to_pos):
+                if move[0] in ["a","b","c","d","e","f","g","h"]:
+                    continue
+                elif int(move[1:]) in range(1,9):
+                    continue
+                raise ValueError
+        except ValueError:
+            print("invalid move")
+            continue
+        if valid_move(from_pos,to_pos):
+            move(from_pos,to_pos)
+
+turn()
